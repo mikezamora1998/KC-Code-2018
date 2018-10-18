b49b2d493bce0ccf19a9358fc8b5bca4d49a8a4c
Michael Zamora <ma.zamora1@pack.csupueblo.edu>
2018-10-17 22:20:55 -0600

    stuff

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
+import store from './store'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
 Vue.use(BootstrapVue)
 
 new Vue({
+  store,
   el: '#app',
   render: h => h(App)
 })
